6b767a1c02d36dbbbe8f23f44a3e009ff7116cab
jannicasavander <jannica.savander@kaikuhealth.com>
2024-02-07 15:48:12 +0200

    exercises 3.2-3.6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-let numbers = [
+let persons = [
     {
         name: "Arto Hellas",
         number: "040-123456",
@@ -27,7 +27,61 @@ let numbers = [
 ];
 
 app.get("/api/persons", (request, response) => {
-    response.json(numbers);
+    response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+    let phone_book_entries = persons.length;
+    html_response = `<p>Phonebook has info for ${phone_book_entries} people</p>
+    <p>${new Date()}</p>`;
+    response.send(html_response);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find(person => person.id === id);
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter(person => person.id !== id);
+
+    response.status(204).end();
+});
+
+const generateId = () => {
+    return Math.floor(Math.random() * 1000000);
+};
+
+app.post("/api/persons", (request, response) => {
+    const body = request.body;
+
+    if (!body.number || !body.name) {
+        return response.status(400).json({
+            error: "name or number missing",
+        });
+    }
+
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: "name must be unique",
+        });
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    };
+
+    persons = persons.concat(person);
+
+    response.json(person);
 });
 
 const PORT = 3001;
