6d147a44c5813e3c92f9b2b966ca19fc24d8a890
jannicasavander <jannica.savander@kaikuhealth.com>
2024-04-03 10:52:02 +0300

    3.15-3.18

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 const Person = require("./models/people");
-const mongoose = require("mongoose");
 
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
-app.use(express.static("dist"));
 
 morgan.token("body", function (req, res) {
     return JSON.stringify(req.body);
@@ -34,7 +33,7 @@ app.get("/info", (request, response) => {
     });
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
     const id = request.params.id;
     Person.findById(id)
         .then(person => {
@@ -44,24 +43,34 @@ app.get("/api/persons/:id", (request, response) => {
                 response.status(404).end();
             }
         })
-        .catch(error => {
-            console.log(error);
-            response.status(500).end();
-        });
+        .catch(error => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(person => person.id !== id);
-
-    response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+    const id = request.params.id;
+    Person.findByIdAndDelete(id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
 });
 
-const generateId = () => {
-    return Math.floor(Math.random() * 1000000);
-};
+app.put("/api/persons/:id", (request, response, next) => {
+    const body = request.body;
 
-app.post("/api/persons", (request, response) => {
+    const person = {
+        name: body.name,
+        number: body.number,
+    };
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson);
+        })
+        .catch(error => next(error));
+});
+
+app.post("/api/persons", (request, response, next) => {
     const body = request.body;
 
     if (!body.number || !body.name) {
@@ -86,11 +95,20 @@ app.post("/api/persons", (request, response) => {
                 response.json(savedPerson);
             });
         })
-        .catch(error => {
-            console.log(error);
-            response.status(500).end();
-        });
+        .catch(error => next(error));
 });
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "malformatted id" });
+    }
+
+    next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
